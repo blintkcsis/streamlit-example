6d27c8bebad3a109416e971c8e07641d33cc6fec
Balint Kocsis <45124913+blintkcsis@users.noreply.github.com>
2023-06-08 08:50:30 +0100

    Retrying csv import

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,10 +7,11 @@ st.set_page_config(page_title="Python Talks Search Engine", page_icon="🐍", la
 st.title("Python Talks Search Engine")
 
 # Connect to the Google Sheet
-sheet_id = "1nctiWcQFaB5UlIs6z8d1O6ZgMHFDMAoo3twVxYnBUws"
-sheet_name = "charlas"
-url = f"https://docs.google.com/spreadsheets/d/1TyeydBGOvcDrSm_OZZqfgI0Xpc0e2mQMXfZvgH8xsUc/edit#gid=650902925"
+sheet_id = "1TyeydBGOvcDrSm_OZZqfgI0Xpc0e2mQMXfZvgH8xsUc"
+sheet_name = "slack"
+url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
 df = pd.read_csv(url, dtype=str).fillna("")
+
 
 # Use a text_input to get the keywords to filter the dataframe
 text_search = st.text_input("Search for information across apps", value="")
