87c9f1e264e468130d1b85b6f179889490336f2d
Balint Kocsis <45124913+blintkcsis@users.noreply.github.com>
2023-06-08 09:04:41 +0100

    Adding cards

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,12 +19,23 @@ text_search = st.text_input("Search for information across apps", value="")
 # Filter the dataframe using masks
 m1 = df["Author"].str.contains(text_search)
 m2 = df["Timestamp"].str.contains(text_search)
-df_search = df[m1 | m2]
+m3 = df["Text"].str.contains(text_search)
+df_search = df[m1 | m2 | m3]
 
 # Another way to show the filtered results
 # Show the cards
+N_cards_per_row = 3
 if text_search:
-    st.write(df_search)
-        
+    for n_row, row in df_search.reset_index().iterrows():
+        i = n_row%N_cards_per_row
+        if i==0:
+            st.write("---")
+            cols = st.columns(N_cards_per_row, gap="large")
+        # draw the card
+        with cols[n_row%N_cards_per_row]:
+            st.caption(f"Slack - {row['Timestamp'].strip()}")
+            st.markdown(f"**{row['Author'].strip()}**")
+            st.markdown(f"*{row['Text'].strip()}*")
+            
 
 
