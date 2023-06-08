854cc7540d3b182cba9481784b4d789d5376ddc9
Balint Kocsis <45124913+blintkcsis@users.noreply.github.com>
2023-06-08 08:11:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,28 +13,17 @@ url = f"https://docs.google.com/spreadsheets/d/1TyeydBGOvcDrSm_OZZqfgI0Xpc0e2mQM
 df = pd.read_csv(url, dtype=str).fillna("")
 
 # Use a text_input to get the keywords to filter the dataframe
-text_search = st.text_input("Search videos by title or speaker", value="")
+text_search = st.text_input("Search for information across apps", value="")
 
 # Filter the dataframe using masks
-m1 = df["Autor"].str.contains(text_search)
-m2 = df["Título"].str.contains(text_search)
+m1 = df["Author"].str.contains(text_search)
+m2 = df["Timestamp"].str.contains(text_search)
 df_search = df[m1 | m2]
 
 # Another way to show the filtered results
 # Show the cards
-N_cards_per_row = 3
 if text_search:
-    for n_row, row in df_search.reset_index().iterrows():
-        i = n_row%N_cards_per_row
-        if i==0:
-            st.write("---")
-            cols = st.columns(N_cards_per_row, gap="large")
-        # draw the card
-        with cols[n_row%N_cards_per_row]:
-            st.caption(f"{row['Evento'].strip()} - {row['Lugar'].strip()} - {row['Fecha'].strip()} ")
-            st.markdown(f"**{row['Autor'].strip()}**")
-            st.markdown(f"*{row['Título'].strip()}*")
-            st.markdown(f"**{row['Video']}**")
+    st.write(df_search)
         
 
 
